b05d684d039b9caea127a4a556e810eb26b210dc
Eric Barb <eric.barb@gmail.com>
2020-06-23 13:14:06 -0400

    Adding Scully

---
+++ scully.NG-Scully-NetlifyCMS.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "NG-Scully-NetlifyCMS",
+  outDir: './dist/static',
+  routes: {
+  }
+};
